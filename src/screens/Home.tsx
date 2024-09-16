fd00c621ec034b6cdbb7a1c7874d59371c1014c8
Timmy Jose <zoltan.jose@gmail.com>
2024-09-16 20:10:37 +0530

    chore: test update 1.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -9,6 +9,7 @@ const Home = () => {
 
   return (
     <View style={styles.container}>
+      <Text>Test Update 1</Text>
       <Pressable
         style={styles.button}
         onPress={() => navigaton.navigate('DummyScreen')}>
